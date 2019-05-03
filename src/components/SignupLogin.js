a18d132876295e9616a5086944468e6169eca183
Jaguso <jagutzo@gmail.com>
2019-05-03 16:11:42 -0500

    made Login and Signup components inside the same component

--- src/components/SignupLogin.js
+++ src/components/SignupLogin.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Signup from './signup/Signup';
+import Login from './login/Login';
+import './SignupLogin.css';
 
 class SignupLogin extends Component {
   render() {
     return (
-      <div>
-        Signup
+      <div className="signuplogin-container">
+        <div className="signup-container">
+          <Signup/>
+        </div>
+        <div className="login-container">
+          <Login/>
+        </div>
       </div>
     );
   }
