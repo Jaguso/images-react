756f97e586baa934a0c34de1ea0e3c9f2c5661b8
Jaguso <jagutzo@gmail.com>
2019-05-04 17:46:24 -0500

    login form made

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
+import './Login.css';
 
 class Login extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  onChangeInput = (event) => {
+    const {name, value} = event.target;
+    console.log("Valores: ", name, value);
+    this.setState({[name]: value});
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log('send');
+  }
+
   render() {
     return (
-      <div>
-        bbbb
+      <div className="login-container">
+        <form onSubmit={this.onSubmit}>
+
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.onChangeInput}
+            />
+          </div>
+
+          <div>
+            <label>Password: </label>
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.onChangeInput}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
       </div>
     );
   }
