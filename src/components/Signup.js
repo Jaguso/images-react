937d7c79a34bc1b429dd119f57a733f24506b895
Jaguso <jagutzo@gmail.com>
2019-05-04 17:32:56 -0500

    inputs made

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
+import './Signup.css';
 
 class Signup extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: ""
+    }
+  }
+
+  onChangeInput = (event) => {
+    const {name, value} = event.target;
+    console.log("Valores: ", name, value);
+    this.setState({[name]: value});
+  }
+
   render() {
     return (
-      <div>
-        AAA
+      <div className="signup-container">
+        <form>
+
+          <div>
+            <label htmlFor="name">Name: </label>
+            <input 
+              type="text" 
+              name="name" 
+              value={this.state.name} 
+              onChange={this.onChangeInput} />
+          </div>
+
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.onChangeInput}
+            />
+          </div>
+
+          <div>
+            <label>Password: </label>
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.onChangeInput}
+            />
+          </div>
+
+        </form>
       </div>
     );
   }
