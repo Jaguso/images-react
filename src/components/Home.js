c264e2b78b90fcc826243b1ec08179383a0c619a
Jaguso <jagutzo@gmail.com>
2019-05-08 17:20:33 -0500

    made service for geting all pictures

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './Home.css';
+import { getAllPictures } from '../services';
 
 class Home extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentWillMount() {
+    getAllPictures().then((response) => {
+      console.log(response);
+      this.setState({
+        data: response.data
+      })
+
+    })
+  }
+
   render() {
     return(
       <div className="home">
-        Home
+        {this.state.data.map(pic => (
+          <div>{pic.description}</div>
+        ))}
       </div>
     );
   }
--- src/services/index.js
+++ src/services/index.js
@@ -6,7 +6,10 @@ const createUser = (data) => axios.post(`${API_URL}/user/signup`, data);
 
 const loginUser = (data) => axios.post(`${API_URL}/user/login`, data);
 
+const getAllPictures = () => axios.get(`${API_URL}/picture`);
+
 export {
   createUser,
-  loginUser
+  loginUser,
+  getAllPictures
 }
