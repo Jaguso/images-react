65a6fe649917156e8a2c3d8fb1529ac3037fec71
Jaguso <jagutzo@gmail.com>
2019-05-16 14:27:28 -0500

    you can now display a single picture

--- src/components/PictureWatch.js
+++ src/components/PictureWatch.js
@@ -7,27 +7,31 @@ class PictureWatch extends Component {
     super(props);
     this.state = {
       id: props.match.params.id,
-      data: []
+      data: {},
+      pic: ""
     }
   }
 
   componentDidMount() {
+    console.log(this.state.id)
     getPicture(this.state.id).then(response => {
+      console.log(response.data);
       this.setState({
-        data: response.data
+        data: response.data,
+        pic: response.data.picture[0]
       });
-      console.log(data);
+      console.log(this.state)
     }).catch(e => {
       console.log(e);
     })
   }
 
   render() {
-    console.log(this.state.data)
+    console.log(this.state.data.picture)
     return(
       <div>
-        dfpog
-        {/* <img src={this.state.data.picture[0]}/> */}
+        sdfg
+        <img src={this.state.pic} width="500" height="400"/>
       </div>
     );
   }
