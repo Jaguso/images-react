56ac386e06813c765e9fa392482ff669bbf36386
Jaguso <jagutzo@gmail.com>
2019-05-18 14:40:41 -0500

    made router redirect to login if you want to create a picture and are not logged in

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import isAuthenticated from './resolvers/isAuthenticated';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import ImageUpload from './components/ImageUpload';
 import PictureWatch from './components/PictureWatch';
+
+
+const PrivateRoute = ({component: Component, ...rest}) => (
+  <Route {...rest} render = {(props) => (
+    isAuthenticated() ? <Component {...props}/> : <Redirect to="/login"/>
+  )} />
+);
 
 class Routes extends Component {
   render() {
@@ -16,7 +24,7 @@ class Routes extends Component {
         <Route exact path="/" component={Home}/>
         <Route exact path="/signup" component={Signup}/>
         <Route exact path="/login" component={Login}/>
-        <Route exact path="/upload" component={ImageUpload}/>
+        <PrivateRoute exact path="/upload" component={ImageUpload}/>
         <Route exact path="/pictures/:id" component={PictureWatch}/>
       </main>
     </BrowserRouter>
